c2929d127703e728ff8a17eb73b01c6b1f401914
valeee.sh <21060480+Valeeesh@users.noreply.github.com>
2022-07-07 23:24:02 +0200

    Add files via upload

---
+++ J.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class J {
+public static void main(String[] args) {
+    
+Scanner input=new Scanner(System.in);
+
+    System.out.println("Inserisci grandezza PiPi");
+    int pp;
+    pp=input.nextInt();
+
+if(pp<=3){
+    System.out.println("Devi compensare");
+}
+
+else 
+{
+    System.out.println("non compensi neanche il cazzo");
+}
+}
+}
+
