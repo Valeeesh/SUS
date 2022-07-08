2ec4832cb62998761a888222df954d7e3622fb5b
valeee.sh <21060480+Valeeesh@users.noreply.github.com>
2022-07-08 13:39:33 +0200

    Update J.java

--- J.java
+++ J.java
@@ -1,21 +1,35 @@
 import java.util.Scanner;
+
 public class J {
-public static void main(String[] args) {
-    
-Scanner input=new Scanner(System.in);
+    public static void main(String[] args) {
 
-    System.out.println("Inserisci grandezza PiPi");
-    int pp;
-    pp=input.nextInt();
+        Scanner input = new Scanner(System.in);
 
-if(pp<=3){
-    System.out.println("Devi compensare");
-}
+        int pett;
 
-else 
-{
-    System.out.println("non compensi neanche il cazzo");
-}
-}
-}
+        System.out.println("Pug top ha perso?, se si 1 se no 0");
+        pett = input.nextInt();
 
+        System.out.println("Inserisci grandezza PiPi");
+        int pp;
+        pp = input.nextInt();
+
+        if (pp <= 3) {
+            System.out.println("Devi compensare per il pp small");
+        }
+
+        else {
+            System.out.println("non compensi neanche il cazzo");
+        }
+
+        if (pett == 1) {
+
+            System.out.println("Pug clash non giochi top vai giungla");
+
+        } else if (pett == 0) {
+
+            System.out.println("Pug al clash non giochi top lo stesso");
+
+        }
+    }
+}
